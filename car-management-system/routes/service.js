b355581f032e4eb3707b393a70898cdff71a2a01
Nishnath12 <143270993+Nishnath12@users.noreply.github.com>
2026-09-01 20:45:32 +0530

    Phase 2: secure customer service history routes

--- car-management-system/routes/service.js
+++ car-management-system/routes/service.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const serviceController = require("../controllers/serviceController");
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.post("/", authMiddleware.verifyToken, serviceController.addService);
-router.get("/", authMiddleware.verifyToken, authMiddleware.checkEmployee, serviceController.getAllServices);
-router.get("/user/:userId", authMiddleware.verifyToken, serviceController.getServicesByUserId);
-router.get("/:id", authMiddleware.verifyToken, authMiddleware.checkEmployee, serviceController.getServiceById);
-router.get("/location/:locationId", authMiddleware.verifyToken, authMiddleware.checkEmployee, serviceController.getServicesByLocationId);
-router.put("/:id/status", authMiddleware.verifyToken, authMiddleware.checkEmployee, serviceController.updateServiceStatus);
-router.put("/:id", authMiddleware.verifyToken, authMiddleware.checkEmployee, serviceController.updateService);
-router.delete("/:id", authMiddleware.verifyToken, authMiddleware.checkEmployee, serviceController.deleteService);
+router.use(authMiddleware.verifyToken);
+router.post("/", serviceController.addService);
+router.get("/user/:userId", serviceController.getServicesByUserId);
+router.get("/", authMiddleware.checkEmployee, serviceController.getAllServices);
+router.get("/location/:locationId", authMiddleware.checkEmployee, serviceController.getServicesByLocationId);
+router.get("/:id", serviceController.getServiceById);
+router.put("/:id/status", authMiddleware.checkEmployee, serviceController.updateServiceStatus);
+router.put("/:id", authMiddleware.checkEmployee, serviceController.updateService);
+router.delete("/:id", authMiddleware.checkEmployee, serviceController.deleteService);
 
 module.exports = router;
