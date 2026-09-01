4a14df45649262aaa566eb77301a5f863af24ab5
Nishnath12 <143270993+Nishnath12@users.noreply.github.com>
2026-09-01 18:11:43 +0530

    Replace broken approval route with working reservation approval API

--- car-management-system/routes/approveBookingsRoutes.js
+++ car-management-system/routes/approveBookingsRoutes.js
@@ -1,14 +1,9 @@
-// routes/sparePartBookingRoutes.js
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/sparePartBookingController');
+const controller = require('../controllers/reservationController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-// Customer routes
-router.post('/', authMiddleware, controller.bookSparePart);
-router.get('/my', authMiddleware, controller.getMyBookings);
-
-// Admin/employee route
-router.get('/', authMiddleware, controller.getAllBookings);
+router.get('/', authMiddleware.verifyToken, authMiddleware.checkEmployee, controller.getReservations);
+router.put('/:id/status', authMiddleware.verifyToken, authMiddleware.checkSupervisor, controller.updateReservation);
 
 module.exports = router;
